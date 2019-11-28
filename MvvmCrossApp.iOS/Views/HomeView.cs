6c391e455f7e721dfca9b7add9a3a29250cafc46
Wasim <muhammad.wasim@purplebricks.com>
2019-11-28 14:07:00 +0500

    HomeView bindings set

--- MvvmCrossApp.iOS/Views/HomeView.cs
+++ MvvmCrossApp.iOS/Views/HomeView.cs
@@ -1,12 +1,13 @@
-﻿using System;
-
-using UIKit;
+﻿using MvvmCross.Binding.BindingContext;
+using MvvmCross.Platforms.Ios.Presenters.Attributes;
+using MvvmCross.Platforms.Ios.Views;
 
 namespace MvvmCrossApp.iOS.Views
 {
-    public partial class HomeView : UIViewController
+    [MvxRootPresentation(WrapInNavigationController = true)]
+    public partial class HomeView : MvxViewController
     {
-        public HomeView() : base("HomeView", null)
+        public HomeView() : base(nameof(HomeView), null)
         {
         }
 
@@ -15,7 +16,11 @@ namespace MvvmCrossApp.iOS.Views
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
 
-           
+            var set = this.CreateBindingSet<HomeView, MvvmCrossApp.Core.ViewModels.HomeViewModel>();
+            set.Bind(TextInput).To(vm => vm.Text);
+            set.Bind(OutputLabel).For(l => l.Text).To(vm => vm.Text);
+            set.Bind(ResetButton).To(vm => vm.ResetTextCommand);
+            set.Apply();
         }
 
         public override void DidReceiveMemoryWarning()
