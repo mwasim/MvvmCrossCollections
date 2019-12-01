00a67b39711c008b176e8c39849e5914e093eb8b
Wasim <muhammad.wasim@purplebricks.com>
2019-12-01 17:03:23 +0500

    TipView implementation

--- MvvmCrossApp.iOS/Views/TipView.cs
+++ MvvmCrossApp.iOS/Views/TipView.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using MvvmCross.Binding.BindingContext;
+using MvvmCross.Platforms.Ios.Presenters.Attributes;
 using MvvmCross.Platforms.Ios.Views;
-using UIKit;
+using MvvmCrossApp.Core.ViewModels;
 
 namespace MvvmCrossApp.iOS.Views
 {
+    [MvxRootPresentation(WrapInNavigationController = true)]
     public partial class TipView : MvxViewController
     {
-        public TipView() : base("TipView", null)
+        public TipView() : base(nameof(TipView), null)
         {
         }
 
@@ -14,6 +16,13 @@ namespace MvvmCrossApp.iOS.Views
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+
+            var set = this.CreateBindingSet<TipView, TipViewModel>();
+
+            set.Bind(SubTotalEditText).To(vm => vm.SubTotal);
+            set.Bind(GenerositySlider).For(slider => slider.Value).To(vm => vm.Generosity);
+            set.Bind(TipLabel).To(vm => vm.Tip);
+            set.Apply();
         }
 
         public override void DidReceiveMemoryWarning()
