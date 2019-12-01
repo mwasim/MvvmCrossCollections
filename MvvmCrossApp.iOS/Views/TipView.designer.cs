e13b1578157b26d38e6fc2e26647681ea2ea6ef9
Wasim <muhammad.wasim@purplebricks.com>
2019-12-01 16:54:28 +0500

    TipView designed in iOS

---
+++ MvvmCrossApp.iOS/Views/TipView.designer.cs
@@ -0,0 +1,47 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace MvvmCrossApp.iOS.Views
+{
+    [Register ("TipView")]
+    partial class TipView
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider GenerositySlider { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField SubTotalEditText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel TipLabel { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (GenerositySlider != null) {
+                GenerositySlider.Dispose ();
+                GenerositySlider = null;
+            }
+
+            if (SubTotalEditText != null) {
+                SubTotalEditText.Dispose ();
+                SubTotalEditText = null;
+            }
+
+            if (TipLabel != null) {
+                TipLabel.Dispose ();
+                TipLabel = null;
+            }
+        }
+    }
+}
