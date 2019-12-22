76444cb8cf34483b5604bc6b6575a9329cd71dab
Wasim <muhammad.wasim@purplebricks.com>
2019-12-22 17:33:53 +0500

    KittenCell created & designed

---
+++ MvvmCrossApp.iOS/Views/Cells/KittenCell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace MvvmCrossApp.iOS.Views.Cells
+{
+    public partial class KittenCell : UITableViewCell
+    {
+        public static readonly NSString Key = new NSString("KittenCell");
+        public static readonly UINib Nib;
+
+        static KittenCell()
+        {
+            Nib = UINib.FromName("KittenCell", NSBundle.MainBundle);
+        }
+
+        protected KittenCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
